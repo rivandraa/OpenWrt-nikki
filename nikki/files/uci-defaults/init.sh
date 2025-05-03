f1d7a5452e2a073d78c8fff529ed0553214aca43
Rivandraa <139041404+rivandraa@users.noreply.github.com>
2025-05-04 00:39:31 +0800

    Update init.sh

--- nikki/files/uci-defaults/init.sh
+++ nikki/files/uci-defaults/init.sh
@@ -2,23 +2,27 @@
 
 . "$IPKG_INSTROOT/etc/nikki/scripts/include.sh"
 
-# check nikki.config.init
-init=$(uci -q get nikki.config.init); [ -z "$init" ] && return
+# Check if nikki.config.init exists and has a value
+init=$(uci -q get nikki.config.init)
 
-# generate random string for api secret and authentication password
+if [ -z "$init" ]; then
+    exit 0  # Exit early if nikki.config.init is not set
+fi
+
+# Generate random string for API secret and authentication password
 random=$(awk 'BEGIN{srand(); print int(rand() * 1000000)}')
 
-# set nikki.mixin.api_secret
+# Set nikki.mixin.api_secret
 uci set nikki.mixin.api_secret="$random"
 
-# set nikki.@authentication[0].password
+# Set nikki.@authentication[0].password
 uci set nikki.@authentication[0].password="$random"
 
-# remove nikki.config.init
+# Remove nikki.config.init
 uci del nikki.config.init
 
-# commit
+# Commit changes to UCI
 uci commit nikki
 
-# exit with 0
+# Exit with 0 (indicating success)
 exit 0
