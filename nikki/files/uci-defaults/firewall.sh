c55db4928313f64396d563b3fabe75306bc4aca4
Rivandraa <139041404+rivandraa@users.noreply.github.com>
2025-05-04 01:22:14 +0800

    Update firewall.sh

--- nikki/files/uci-defaults/firewall.sh
+++ nikki/files/uci-defaults/firewall.sh
@@ -2,11 +2,11 @@
 
 . "$IPKG_INSTROOT/etc/nikki/scripts/include.sh"
 
-uci -q batch <<EOF > /dev/null
-del firewall.nikki
-set firewall.nikki=include
-set firewall.nikki.type=script
-set firewall.nikki.path="$FIREWALL_INCLUDE_SH"
-set firewall.nikki.fw4_compatible=1
-commit firewall
+uci -q batch <<-EOF > /dev/null
+	del firewall.nikki
+	set firewall.nikki=include
+	set firewall.nikki.type=script
+	set firewall.nikki.path=$FIREWALL_INCLUDE_SH
+	set firewall.nikki.fw4_compatible=1
+	commit firewall
 EOF
