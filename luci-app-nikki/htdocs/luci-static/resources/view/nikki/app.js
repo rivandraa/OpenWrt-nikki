08413562e7b1ec20b0800256ba12850f68fa031d
Rivandraa <139041404+rivandraa@users.noreply.github.com>
2025-05-12 16:54:48 +0800

    Update app.js

--- luci-app-nikki/htdocs/luci-static/resources/view/nikki/app.js
+++ luci-app-nikki/htdocs/luci-static/resources/view/nikki/app.js
@@ -6,13 +6,13 @@
 'require tools.nikki as nikki';
 
 function renderStatus(running) {
-    return updateStatus(E('input', { id: 'core_status', style: 'border: unset; font-style: italic; font-weight: bold;', readonly: '' }), running);
+    return updateStatus(E('div', { id: 'core_status', style: 'border: unset; font-style: italic; font-weight: bold;', readonly: '' }), running);
 }
 
 function updateStatus(element, running) {
     if (element) {
         element.style.color = running ? 'green' : 'red';
-        element.value = running ? _('Running') : _('Not Running');
+        element.textContent = running ? _('Running') : _('Not Running');
     }
     return element;
 }
@@ -26,38 +26,36 @@ return view.extend({
             nikki.listProfiles()
         ]);
     },
+
     render: function (data) {
         const subscriptions = uci.sections('nikki', 'subscription');
-        const appVersion = data[1].app ?? '';
-        const coreVersion = data[1].core ?? '';
-        const running = data[2];
-        const profiles = data[3];
+        const appVersion = (data[1] && data[1].app) ? data[1].app : '';
+        const coreVersion = (data[1] && data[1].core) ? data[1].core : '';
+        const running = (data[2] !== undefined) ? data[2] : false;
+        const profiles = (data[3] !== undefined) ? data[3] : [];
 
         let m, s, o;
 
-        m = new form.Map('nikki', _('Nikki'), `${_('Transparent Proxy with Mihomo on OpenWrt.')} <a href="https://github.com/nikkinikki-org/OpenWrt-nikki/wiki" target="_blank">${_('How To Use')}</a>`);
+        m = new form.Map('nikki');
 
-        s = m.section(form.TableSection, 'status', _('Status'));
+        s = m.section(form.TableSection, 'status', _('📊 Status'));
         s.anonymous = true;
 
-        o = s.option(form.Value, '_app_version', _('App Version'));
+        // App Version
+        o = s.option(form.DummyValue, '_app_version', _('App Version'));
         o.readonly = true;
-        o.load = function () {
-            return appVersion;
-        };
-        o.write = function () { };
+        o.load = () => appVersion;
+        o.write = () => {};
 
-        o = s.option(form.Value, '_core_version', _('Core Version'));
+        // Core Version
+        o = s.option(form.DummyValue, '_core_version', _('Core Version'));
         o.readonly = true;
-        o.load = function () {
-            return coreVersion;
-        };
-        o.write = function () { };
+        o.load = () => coreVersion;
+        o.write = () => {};
 
+        // Core Status
         o = s.option(form.DummyValue, '_core_status', _('Core Status'));
-        o.cfgvalue = function () {
-            return renderStatus(running);
-        };
+        o.cfgvalue = () => renderStatus(running);
         poll.add(function () {
             return L.resolveDefault(nikki.status()).then(function (running) {
                 updateStatus(document.getElementById('core_status'), running);
@@ -67,31 +65,23 @@ return view.extend({
         o = s.option(form.Button, 'reload');
         o.inputstyle = 'action';
         o.inputtitle = _('Reload Service');
-        o.onclick = function () {
-            return nikki.reload();
-        };
+        o.onclick = () => nikki.reload();
 
         o = s.option(form.Button, 'restart');
         o.inputstyle = 'negative';
         o.inputtitle = _('Restart Service');
-        o.onclick = function () {
-            return nikki.restart();
-        };
+        o.onclick = () => nikki.restart();
 
         o = s.option(form.Button, 'update_dashboard');
         o.inputstyle = 'positive';
         o.inputtitle = _('Update Dashboard');
-        o.onclick = function () {
-            return nikki.updateDashboard();
-        };
+        o.onclick = () => nikki.updateDashboard();
 
         o = s.option(form.Button, 'open_dashboard');
         o.inputtitle = _('Open Dashboard');
-        o.onclick = function () {
-            return nikki.openDashboard();
-        };
+        o.onclick = () => nikki.openDashboard();
 
-        s = m.section(form.NamedSection, 'config', 'config', _('App Config'));
+        s = m.section(form.NamedSection, 'config', 'config', _('⚙️ App Config'));
 
         o = s.option(form.Flag, 'enabled', _('Enable'));
         o.rmempty = false;
@@ -101,11 +91,11 @@ return view.extend({
 
         for (const profile of profiles) {
             o.value('file:' + profile.name, _('File:') + profile.name);
-        };
+        }
 
         for (const subscription of subscriptions) {
             o.value('subscription:' + subscription['.name'], _('Subscription:') + subscription.name);
-        };
+        }
 
         o = s.option(form.Value, 'start_delay', _('Start Delay'));
         o.datatype = 'uinteger';
@@ -125,7 +115,7 @@ return view.extend({
         o = s.option(form.Flag, 'fast_reload', _('Fast Reload'));
         o.rmempty = false;
 
-        s = m.section(form.NamedSection, 'env', 'env', _('Core Environment Variable Config'));
+        s = m.section(form.NamedSection, 'env', 'env', _('🛠️ Core Environment Variable Config'));
 
         o = s.option(form.Flag, 'disable_safe_path_check', _('Disable Safe Path Check'));
         o.rmempty = false;
